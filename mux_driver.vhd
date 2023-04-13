21398bd7da478f9ff793a153687507e9041ea987
evvejiji <240689@vutbr.cz>
2023-04-13 17:13:05 +0200

    Edited

--- mux_driver.vhd
+++ mux_driver.vhd
@@ -3,140 +3,71 @@ library ieee;
   use ieee.numeric_std.all;
 
 
-entity button_driver is
+entity mux_driver is
     port(
     clk         : in  std_logic;
-    SW         : in  std_logic;
     rst         : in  std_logic;
-    input       : in  std_logic;
-    send       : in  std_logic;
-    output      : out  std_logic_vector(20 downto 0);
-    LED17_R : out STD_LOGIC;
-    LED17_G : out STD_LOGIC;
-    LED17_B : out STD_LOGIC
+    input       : in  std_logic_vector(20 downto 0);
+    output1      : out  std_logic_vector(20 downto 0);
+    output2      : out  std_logic_vector(20 downto 0);
+    output3      : out  std_logic_vector(20 downto 0);
+    output4      : out  std_logic_vector(20 downto 0);
+    output5      : out  std_logic_vector(20 downto 0);
+    output6      : out  std_logic_vector(20 downto 0);
+    output7      : out  std_logic_vector(20 downto 0);
+    output8      : out  std_logic_vector(20 downto 0)
     );
-end entity button_driver;
+end entity mux_driver;
 
-architecture Behavioral of button_driver is
-    signal sig_out : std_logic_vector(20 downto 0);
-    signal one_cnt : integer := 0;
-    signal zero_cnt : integer := 0;
+architecture Behavioral of mux_driver is
+    signal sig_control : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_1 : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_2 : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_3 : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_4 : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_5 : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_6 : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_7 : std_logic_vector(20 downto 0) := "000000000000000000000";
+    signal sig_8 : std_logic_vector(20 downto 0) := "000000000000000000000";
     begin
-        p_button_driver : process (clk,input,send) is
+        p_mux_driver : process (clk,input) is
             begin
-                if (SW = '1') then
-                    if (rst = '1') then
-                        sig_out <= "000000000000000000000";
-                        output <= (others => '0');
-                        one_cnt <= 0;
-                        LED17_G <= '0';
-                        LED17_B <= '0';
-                        LED17_R <= '0';
-                    elsif rising_edge(clk) then
-                        if (send = '1') then
-                            output <= sig_out;
-                            sig_out <= "000000000000000000000";
-                        end if;
-                        if (input = '0') then
-                            zero_cnt <= zero_cnt + 1;
-                            one_cnt <= 0;
-                            LED17_G <= '1';
-                            LED17_B <= '0';
-                            LED17_R <= '0';
-                        end if;
-                        if (zero_cnt = 1) then
-                            sig_out(20) <= sig_out(19);
-                            sig_out(19) <= sig_out(18);
-                            sig_out(18) <= sig_out(17);
-                            sig_out(17) <= sig_out(16);
-                            sig_out(16) <= sig_out(15);
-                            sig_out(15) <= sig_out(14);
-                            sig_out(14) <= sig_out(13);
-                            sig_out(13) <= sig_out(12);
-                            sig_out(12) <= sig_out(11);
-                            sig_out(11) <= sig_out(10);
-                            sig_out(10) <= sig_out(9);
-                            sig_out(9) <= sig_out(8);
-                            sig_out(8) <= sig_out(7);
-                            sig_out(7) <= sig_out(6);
-                            sig_out(6) <= sig_out(5);
-                            sig_out(5) <= sig_out(4);
-                            sig_out(4) <= sig_out(3);
-                            sig_out(3) <= sig_out(2);
-                            sig_out(2) <= sig_out(1);
-                            sig_out(1) <= sig_out(0);
-                            sig_out(0) <= '0';
-                        end if;
-                        if (input = '1') then
-                            zero_cnt <= 0;
-                            one_cnt <= one_cnt + 1;
-                            if (one_cnt < 2) then
-                                LED17_G <= '0';
-                                LED17_B <= '0';
-                                LED17_R <= '1';
-                            elsif (one_cnt = 2) then
-                                LED17_G <= '0';
-                                LED17_B <= '1';
-                                LED17_R <= '0';
-                            elsif (one_cnt > 2) then
-                                LED17_G <= '0';
-                                LED17_B <= '1';
-                                LED17_R <= '0';
-                            end if;
-                        end if;
-                        if (one_cnt = 1) then
-                            sig_out(20) <= sig_out(19);
-                            sig_out(19) <= sig_out(18);
-                            sig_out(18) <= sig_out(17);
-                            sig_out(17) <= sig_out(16);
-                            sig_out(16) <= sig_out(15);
-                            sig_out(15) <= sig_out(14);
-                            sig_out(14) <= sig_out(13);
-                            sig_out(13) <= sig_out(12);
-                            sig_out(12) <= sig_out(11);
-                            sig_out(11) <= sig_out(10);
-                            sig_out(10) <= sig_out(9);
-                            sig_out(9) <= sig_out(8);
-                            sig_out(8) <= sig_out(7);
-                            sig_out(7) <= sig_out(6);
-                            sig_out(6) <= sig_out(5);
-                            sig_out(5) <= sig_out(4);
-                            sig_out(4) <= sig_out(3);
-                            sig_out(3) <= sig_out(2);
-                            sig_out(2) <= sig_out(1);
-                            sig_out(1) <= sig_out(0);
-                            sig_out(0) <= '1';
-                        elsif (one_cnt = 3) then
-                            sig_out(20) <= sig_out(18);
-                            sig_out(19) <= sig_out(17);
-                            sig_out(18) <= sig_out(16);
-                            sig_out(17) <= sig_out(15);
-                            sig_out(16) <= sig_out(14);
-                            sig_out(15) <= sig_out(13);
-                            sig_out(14) <= sig_out(12);
-                            sig_out(13) <= sig_out(11);
-                            sig_out(12) <= sig_out(10);
-                            sig_out(11) <= sig_out(9);
-                            sig_out(10) <= sig_out(8);
-                            sig_out(9) <= sig_out(7);
-                            sig_out(8) <= sig_out(6);
-                            sig_out(7) <= sig_out(5);
-                            sig_out(6) <= sig_out(4);
-                            sig_out(5) <= sig_out(3);
-                            sig_out(4) <= sig_out(2);
-                            sig_out(3) <= sig_out(1);
-                            sig_out(2) <= sig_out(0);
-                            sig_out(1) <= '1';
-                            sig_out(0) <= '1';
-                        end if;
+                if (rst = '1') then
+                    output1 <= "000000000000000000000";
+                    output2 <= "000000000000000000000";
+                    output3 <= "000000000000000000000";
+                    output4 <= "000000000000000000000";
+                    output5 <= "000000000000000000000";
+                    output6 <= "000000000000000000000";
+                    output7 <= "000000000000000000000";
+                    output8 <= "000000000000000000000";
+                    sig_1   <= "000000000000000000000";
+                    sig_2   <= "000000000000000000000";
+                    sig_3   <= "000000000000000000000";
+                    sig_4   <= "000000000000000000000";
+                    sig_5   <= "000000000000000000000";
+                    sig_6   <= "000000000000000000000";
+                    sig_7   <= "000000000000000000000";
+                elsif rising_edge(clk) then
+                    if input /= sig_control then
+                        sig_8 <= sig_7;
+                        sig_7 <= sig_6;
+                        sig_6 <= sig_5;
+                        sig_5 <= sig_4;
+                        sig_4 <= sig_3;
+                        sig_3 <= sig_2;
+                        sig_2 <= sig_1;
+                        sig_1 <= input;
+                        output1 <= input;
+                        output2 <= sig_1;
+                        output3 <= sig_2;
+                        output4 <= sig_3;
+                        output5 <= sig_4;
+                        output6 <= sig_5;
+                        output7 <= sig_6;
+                        output8 <= sig_7;
+                        sig_control <= input;
                     end if;
-                else 
-                sig_out <= "000000000000000000000";
-                output <= (others => '0');
-                one_cnt <= 0;
-                LED17_G <= '0';
-                LED17_B <= '0';
-                LED17_R <= '0';
                 end if;
-            end process p_button_driver;
+            end process p_mux_driver;
     end architecture Behavioral;
--- tb_mux_driver.vhd
+++ tb_mux_driver.vhd
@@ -3,43 +3,47 @@ library ieee;
   use ieee.numeric_std.all;
 
 
-entity tb_button_driver is
+entity tb_mux_driver is
 --  Port ( );
-end tb_button_driver;
+end tb_mux_driver;
 
-architecture testbench of tb_button_driver is
+architecture testbench of tb_mux_driver is
 
     constant c_CLK_100MHZ_PERIOD : time := 10 ns;
 
-    signal SW                : std_logic;
     signal rst               : std_logic;
     signal sig_clk_100mhz    : std_logic;
-    signal input             : std_logic;
-    signal send              : std_logic;
-    signal output            : std_logic_vector(20 downto 0);
-    signal LED17_R :  STD_LOGIC;
-    signal LED17_G :  STD_LOGIC;
-    signal LED17_B :  STD_LOGIC;
+    signal input             : std_logic_vector(20 downto 0);
+    signal output1           : std_logic_vector(20 downto 0);
+    signal output2           : std_logic_vector(20 downto 0);
+    signal output3           : std_logic_vector(20 downto 0);
+    signal output4           : std_logic_vector(20 downto 0);
+    signal output5           : std_logic_vector(20 downto 0);
+    signal output6           : std_logic_vector(20 downto 0);
+    signal output7           : std_logic_vector(20 downto 0);
+    signal output8           : std_logic_vector(20 downto 0);
 
 
 begin
-    uut_button_driver: entity work.button_driver
+    uut_mux_driver: entity work.mux_driver
         port map(
-            SW         => SW,
             rst        => rst,
             clk        => sig_clk_100mhz,
             input      => input,
-            output     => output,
-            send       => send,
-            LED17_R    => LED17_R,
-            LED17_G    => LED17_G,
-            LED17_B    => LED17_B
+            output1    => output1,
+            output2    => output2,
+            output3    => output3,
+            output4    => output4,
+            output5    => output5,
+            output6    => output6,
+            output7    => output7,
+            output8    => output8
         );
         
     p_clk_gen : process is
     begin
           
-        while now < 2000 ns loop -- 200 periods of 100MHz clock
+        while now < 1000 ns loop -- 100 periods of 100MHz clock
           
              sig_clk_100mhz <= '0';
              wait for c_CLK_100MHZ_PERIOD / 2;
@@ -59,144 +63,32 @@ begin
         rst <= '1';
         wait for 90 ns;
         rst <= '0';
-        wait for 1500 ns;
-        rst <= '1';
-        wait for 90 ns;
-        rst <= '0';
+        wait for 10 ns;
         wait;
       
     end process p_reset_gen;
 
-    p_SW : process is
-        begin
-            SW <= '1';
-            wait;
-        end process p_SW;
-
-    p_send : process is
-        begin
-          
-            send <= '0';
-            wait for 300 ns;
-            send <= '1';
-            wait for 10 ns;
-            send <= '0';
-            wait for 220 ns;
-            send <= '1';
-            wait for 10 ns;
-            send <= '0';
-            wait for 220 ns;
-            send <= '1';
-            wait for 10 ns;
-            send <= '0';
-            wait for 190 ns;
-            send <= '1';
-            wait for 10 ns;
-            send <= '0';
-            wait for 200 ns;
-            send <= '1';
-            wait for 10 ns;
-            send <= '0';
-            wait for 370 ns;
-            send <= '1';
-            wait for 10 ns;
-            send <= '0';
-            wait;
-          
-    end process p_send;
 
     p_stimulus : process
     begin
-    	input <= '0';
-        wait for 200 ns; -- NO INPUT
-        input <= '1';
-        wait for 25 ns; --longer
-        input <= '0';
-        wait for 10 ns; -- A
-        input <= '1';
-        wait for 50 ns; --longer
-        input <= '0';
-        wait for 100 ns; -- Pause --longer
-        input <= '1';
-        wait for 30 ns;
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 25 ns; --longer
-        input <= '0';
-        wait for 10 ns; -- B
-        input <= '1';
-        wait for 10 ns;
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 25 ns; --longer
-        input <= '0';
-        wait for 50 ns; -- Pause --longer
-        input <= '1';
-        wait for 30 ns;
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 25 ns; --longer
-        input <= '0';
-        wait for 10 ns; -- C
-        input <= '1';
-        wait for 60 ns; --longer
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 27 ns; --longer
-        input <= '0';
-        wait for 70 ns; -- Pause --longer
-        input <= '1';
-        wait for 25 ns; --longer
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 30 ns;
-        input <= '0';
-        wait for 10 ns; -- J
-        input <= '1';
-        wait for 30 ns;
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 30 ns;
-        input <= '0';
-        wait for 50 ns; -- Pause --longer
-        input <= '1';
-        wait for 30 ns;
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 60 ns; -- O --longer
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 50 ns; --longer
-        input <= '0';
-        wait for 30 ns; -- Pause
-        input <= '1';
-        wait for 30 ns;
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 50 ns; -- 0 --longer
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 70 ns; --longer
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 30 ns; 
-        input <= '0';
-        wait for 10 ns;
-        input <= '1';
-        wait for 70 ns; --longer
-        input <= '0';
-        wait for 150 ns; -- Pause --longer
+        input <= "111000000000000000111";
+        wait for 50 ns;
+    	input <= "111000000000000000000";
+        wait for 200 ns;
+        input <= "111000000000000111111";
+        wait for 100 ns;
+        input <= "111000000000111000111";
+        wait for 100 ns;
+        input <= "111000000000111111111";
+        wait for 100 ns;
+        input <= "111000000111000000111";
+        wait for 100 ns;
+        input <= "111000000111111000111";
+        wait for 100 ns;
+        input <= "111000111000000000111";
+        wait for 100 ns;
+        input <= "111000111000111000111";
+        wait for 100 ns;
         wait;
     end process p_stimulus;
 end architecture testbench;
